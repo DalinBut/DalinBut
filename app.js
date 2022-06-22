7238b2a6a437d8dd268ec1d13197e538b5c721b8
But Dalin <butdalin25@gmail.com>
2022-06-22 13:09:37 +0700

    add starts

--- app.js
+++ app.js
@@ -3,7 +3,13 @@ const app = require('express')();
 const port = process.env.PORT || 3000;
 
 app.get("", (req, res) => {
-    res.send("Hello");
+    // res.send("Hello");
+    res.json({
+        posts: [
+            {title: 'First post'},
+            {title: 'Second post'}
+        ]
+    });
 })
 
 app.listen(port,(req, res) => {
